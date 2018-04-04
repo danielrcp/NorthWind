2594bd712b78920c823234e9850adfb3cb170963
Daniel <daricaba@hotmail.com>
2018-04-04 04:42:08 +0200

    Versión con ejemplos de manejo de información entre vistas

---
+++ NorthWind/Controllers/PersonaController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace NorthWind.Controllers
+{
+    public class PersonaController : Controller
+    {
+        private SaludMovilEntities db = new SaludMovilEntities();
+
+        // GET: Persona
+        public async Task<ActionResult> Index()
+        {
+            var sm_Persona = db.sm_Persona.Include(s => s.sm_Paciente).Include(s => s.sm_Paciente1).Include(s => s.sm_PacientePrograma);
+            return View(await sm_Persona.ToListAsync());
+        }
+
+        // GET: Persona/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sm_Persona sm_Persona = await db.sm_Persona.FindAsync(id);
+            if (sm_Persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sm_Persona);
+        }
+
+        // GET: Persona/Create
+        public ActionResult Create()
+        {
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_Paciente, "idTipoIdentificacion", "segmento");
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_Paciente, "idTipoIdentificacion", "segmento");
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_PacientePrograma, "idTipoIdentificacion", "observaciones");
+            return View();
+        }
+
+        // POST: Persona/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "idTipoIdentificacion,numeroIdentificacion,primerNombre,segundoNombre,primerApellido,segundoApellido,idTipo,fechaNacimiento,idCiudad,celular,telefonoFijo,correo,createdDate,createdBy,updatedDate,updatedBy")] sm_Persona sm_Persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.sm_Persona.Add(sm_Persona);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_Paciente, "idTipoIdentificacion", "segmento", sm_Persona.idTipoIdentificacion);
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_Paciente, "idTipoIdentificacion", "segmento", sm_Persona.idTipoIdentificacion);
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_PacientePrograma, "idTipoIdentificacion", "observaciones", sm_Persona.idTipoIdentificacion);
+            return View(sm_Persona);
+        }
+
+        // GET: Persona/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sm_Persona sm_Persona = await db.sm_Persona.FindAsync(id);
+            if (sm_Persona == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_Paciente, "idTipoIdentificacion", "segmento", sm_Persona.idTipoIdentificacion);
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_Paciente, "idTipoIdentificacion", "segmento", sm_Persona.idTipoIdentificacion);
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_PacientePrograma, "idTipoIdentificacion", "observaciones", sm_Persona.idTipoIdentificacion);
+            return View(sm_Persona);
+        }
+
+        // POST: Persona/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "idTipoIdentificacion,numeroIdentificacion,primerNombre,segundoNombre,primerApellido,segundoApellido,idTipo,fechaNacimiento,idCiudad,celular,telefonoFijo,correo,createdDate,createdBy,updatedDate,updatedBy")] sm_Persona sm_Persona)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sm_Persona).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_Paciente, "idTipoIdentificacion", "segmento", sm_Persona.idTipoIdentificacion);
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_Paciente, "idTipoIdentificacion", "segmento", sm_Persona.idTipoIdentificacion);
+            ViewBag.idTipoIdentificacion = new SelectList(db.sm_PacientePrograma, "idTipoIdentificacion", "observaciones", sm_Persona.idTipoIdentificacion);
+            return View(sm_Persona);
+        }
+
+        // GET: Persona/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            sm_Persona sm_Persona = await db.sm_Persona.FindAsync(id);
+            if (sm_Persona == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sm_Persona);
+        }
+
+        // POST: Persona/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            sm_Persona sm_Persona = await db.sm_Persona.FindAsync(id);
+            db.sm_Persona.Remove(sm_Persona);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NorthWind/Controllers/ProgramaController.cs
@@ -0,0 +1,84 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWind.Controllers
+{
+    public class ProgramaController : Controller
+    {
+        // GET: Programa
+        public ActionResult Index()
+        {
+            var Context = new Model.SaludMovilEntities();
+            return View(Context.sm_Programa.ToList());
+        }
+
+        public JsonResult Details(int id)
+        {
+            JsonResult resultado;
+            var Context = new SaludMovilEntities();
+            return resultado = Json(Context.sm_Programa.Find(id),JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Message = "Proporciones los datos del programa";
+            ViewBag.TimeServer = DateTime.UtcNow;
+            sm_Programa programa = new sm_Programa();
+            var Context = new SaludMovilEntities();
+            var Estados = Context.sm_Estado.Select(e => new { e.idEstado,e.nombre });
+            ViewBag.idEstado = new SelectList(Estados,"idEstado","nombre");
+            var Poblaciones = Context.sm_Poblacion.Select(p => new { p.idPoblacion , p.descripcion });
+            ViewBag.poblacionObjetivo = new SelectList(Poblaciones, "idPoblacion", "descripcion");
+            return View(programa);
+        }
+        [HttpPost]
+        public ActionResult Create(sm_Programa programa)
+        {
+            ActionResult Result;
+            
+            if (ModelState.IsValid)
+            {
+                var Context = new SaludMovilEntities();
+                Context.sm_Programa.Add(programa);
+                Context.SaveChanges();
+                Result = RedirectToAction("Details",new { id = programa.idPrograma});
+            }
+            else
+            {
+                Result = View(programa);
+            }
+            return Result;
+        }
+
+        public ActionResult NoAutorizado() => new HttpUnauthorizedResult("Acceso denegado");
+        public ActionResult HacerAlgoSinRespuesta() => new EmptyResult();
+        [ChildActionOnly]
+        public ActionResult Total()
+        {
+            var Context = new SaludMovilEntities();
+            var Result = string.Format($"<strong>{Context.sm_Programa.Count()} productos </strong>");
+            return Content(Result);
+        }
+
+        public ActionResult GetProgramaByName(string name)
+        {
+            ActionResult Result;
+
+            var Context = new SaludMovilEntities();
+            var Programa = Context.sm_Programa.FirstOrDefault(p => p.descripcion.Contains(name));
+            if (Programa != null)
+            {
+                Result = View("Details",Programa);
+            }
+            else
+            {
+                Result = HttpNotFound($"Producto {name} no encontrado");
+            }
+            return Result;
+        }
+    }
+}
